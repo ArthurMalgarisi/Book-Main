e8e52bb63865617be5b53168d40ca6aaad451bc5
Arthur Malgarisi Aguiar <68798832+ArthurMalgarisi@users.noreply.github.com>
2024-09-06 11:22:57 -0300

    firts commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import BookList from "./BookList";
+import BookContextProvider from "../context/BookContext";
+import ThemeContextProvider from "../context/ThemeContext";
+export default class App extends React.Component {
+  render() {
+    return (
+      <ThemeContextProvider>
+        <BookContextProvider>
+          <BookList />
+        </BookContextProvider>
+      </ThemeContextProvider>
+    );
+  }
+}
